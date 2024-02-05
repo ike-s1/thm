61cdddab195a75981019dbaceef197517523333e
ike-s1 <vitalik.fedorchak1111#gmail.com>
2024-02-05 11:49:04 +0200

    adaptive v2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./Header.scss";
 import logoBlack from "../../assets/logo-black.svg";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import menuIcon from "../../assets/menu-icon.svg";
+import { CustomButton } from "../Shared/CustomBtn/CustomBtn";
 
 const Header = () => {
   const navElements = ["About", "Features", "User Cases", "Compare Platforms"];
@@ -12,10 +13,20 @@ const Header = () => {
 
   const handleMenuClick = () => {
     setActiveMenu((prev) => !prev);
+    if (!activeMenu) {
+      document.querySelector('header').classList.add('closed');
+    } else {
+      document.querySelector('header').classList.remove('closed');
+    }
   };
 
+  const handleJoinClick = () => {
+    setActiveMenu(false);
+    window.location.href = "#Book"
+  }
+
   return (
-    <header>
+    <header className={activeMenu ? "active" : "closed"}>
       <div className="header-content">
         <Link to="/">
           <img src={logoBlack} className="header-logo" alt="logo" />
@@ -29,6 +40,7 @@ const Header = () => {
               </a>
             </p>
           ))}
+        <CustomButton text="Join Waitlist" className="join-btn" onClick={handleJoinClick}/>
         </nav>
         <button className={`get-started-btn ${isHomePage ? "" : "hidden"}`}>
           <a href="#Book">Get Started for Free</a>
@@ -40,6 +52,7 @@ const Header = () => {
           alt="menu-icon"
         />
       </div>
+      <p className="all-rights-header">©2024 All rights reserved.</p>
     </header>
   );
 };
